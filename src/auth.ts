d2f948c58988d7f307b8ab4e66295a2fc5407d72
tomo <asds7000@gmail.com>
2025-11-16 19:13:33 +0900

    test

--- src/auth.ts
+++ src/auth.ts
@@ -11,23 +11,5 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
 		})
 	],
 	secret: AUTH_SECRET,
-	trustHost: true,
-
-	// ▼▼▼ このcookiesブロックを追加 ▼▼▼
-	cookies: {
-		sessionToken: {
-			name: `__Secure-authjs.session-token`,
-			options: {
-				httpOnly: true,
-				sameSite: 'lax',
-				path: '/',
-				secure: true,
-				// Vercelのプレビュードメインと本番ドメインで共有するためにDomain属性を調整
-				// process.env.VERCEL_URLにはVercelが自動で現在のURLを入れてくれる
-				domain: process.env.VERCEL_URL
-					? `.${process.env.VERCEL_URL.split('.').slice(-2).join('.')}`
-					: undefined
-			}
-		}
-	}
+	trustHost: true
 });
